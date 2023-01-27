d2f2ba771b5142727e35eaa1009ead090c24157e
Pasha Kovalskiy <kovalskyi08@gmail.com>
2023-01-27 22:32:04 +0200

    Added path object to config

---
+++ gulp/config/path.js
@@ -0,0 +1,20 @@
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const distFolder = './dist';
+const srcFolder = './src';
+
+export const path = {
+   dist: {
+      files: `${distFolder}/files/`,
+   },
+   src: {
+      files: `${srcFolder}/files/**/*.*`,
+   },
+   watch: {},
+   clean: distFolder,
+   distFolder,
+   srcFolder,
+   rootFolder,
+   ftp: ``,
+};
