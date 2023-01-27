be5fde55a564b98a95240073a1628dfdf80f7a41
Pasha Kovalskiy <kovalskyi08@gmail.com>
2023-01-27 23:09:24 +0200

    Added copy task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 // Gulp
 import gulp from 'gulp';
 // Path import
-import { path } from './gulp/config/path';
+import { path } from './gulp/config/path.js';
 
 // Global object of our gulp assembly
 global.app = {
    path,
    gulp,
 };
+
+// Tasks import
+import { copy } from './gulp/tasks/copy.js';
+
+// Assembly gulp in action
+gulp.task('copy', copy);
