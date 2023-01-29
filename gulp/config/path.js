262d65a20704b5ab5cb4451a0bfa02c9e2d887b3
Pasha Kovalskiy <kovalskyi08@gmail.com>
2023-01-29 16:53:04 +0200

    Added html task

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -6,12 +6,15 @@ const srcFolder = './src';
 
 export const path = {
    dist: {
+      html: `${distFolder}/`,
       files: `${distFolder}/files/`,
    },
    src: {
+      html: `${srcFolder}/*.html`,
       files: `${srcFolder}/files/**/*.*`,
    },
    watch: {
+      html: `${srcFolder}/**/*.html`,
       files: `${srcFolder}/files/**/*.*`,
    },
    clean: distFolder,
--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,32 @@
 import gulp from 'gulp';
 // Path import
 import { path } from './gulp/config/path.js';
+//Import common plugins
+import { plugins } from './gulp/config/plugins.js';
 
 // Global object of our gulp assembly
 global.app = {
    path,
    gulp,
+   plugins,
 };
 
 // Tasks import
 import { copy } from './gulp/tasks/copy.js';
 import { reset } from './gulp/tasks/reset.js';
+import { html } from './gulp/tasks/html.js';
 
-const refreshFolderTask = gulp.series(reset, copy);
+const copyFiles = gulp.parallel(copy, html);
+const mainTask = gulp.series(reset, copyFiles);
 
 // Watcher
 const watcher = () => {
-   gulp.watch(path.watch.files, refreshFolderTask);
+   gulp.watch(path.watch.files, copyFiles);
+   gulp.watch(path.watch.html, html);
 };
 
 // Scripts for executing tasks
-const mainTask = gulp.series(refreshFolderTask, watcher);
+const dev = gulp.series(mainTask, watcher);
 
 // Assembly gulp in action
-gulp.task('default', mainTask);
+gulp.task('default', dev);
