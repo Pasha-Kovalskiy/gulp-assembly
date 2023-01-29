f6f41cb851705cd75bd1ddadf760c02ae9d9a4ad
Pasha Kovalskiy <kovalskyi08@gmail.com>
2023-01-29 20:53:03 +0200

    Added images task

--- gulpfile.js
+++ gulpfile.js
@@ -19,8 +19,9 @@ import { html } from './gulp/tasks/html.js';
 import { server } from './gulp/tasks/server.js';
 import { scss } from './gulp/tasks/scss.js';
 import { js } from './gulp/tasks/js.js';
+import { images } from './gulp/tasks/images.js';
 
-const copyFiles = gulp.parallel(copy, html, scss, js);
+const copyFiles = gulp.parallel(copy, html, scss, js, images);
 const mainTask = gulp.series(reset, copyFiles);
 
 // Watcher
@@ -29,6 +30,7 @@ const watcher = () => {
    gulp.watch(path.watch.html, html);
    gulp.watch(path.watch.scss, scss);
    gulp.watch(path.watch.js, js);
+   gulp.watch(path.watch.images, images);
 };
 
 // Scripts for executing tasks
